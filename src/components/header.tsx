73be41cb8c6651863a3c9caca196a87eaf86e2ba
Filipe Andujar <filipeandujar@gmail.com>
2023-12-14 13:23:25 -0300

    feat: separate kftray and header component

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import {
+    Heading,
+    Image,
+  } from "@chakra-ui/react"
+import logo from "./../logo.png"
+
+const Header = () => {
+return (
+    <Heading
+            as="h1"
+            size="lg"
+            color="white"
+            mb={0}
+            marginTop={-2}
+            background="transparent"
+          >
+            <Image boxSize="80px" src={logo} />
+    </Heading>
+    );
+}
+
+export {
+    Header
+}
