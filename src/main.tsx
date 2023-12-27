3647b33e038b24e6be2cc83933bd750ff2166b53
hcavarsan <hcavarsan@yahoo.com.br>
2023-12-27 14:24:46 -0300

    feat: add autocomplete fields

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query' // Import QueryClient and QueryClientProvider
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { attachConsole } from 'tauri-plugin-log-api'
 
 import { ChakraProvider } from '@chakra-ui/react'
@@ -14,10 +14,8 @@ if (import.meta.env.DEV) {
   attachConsole()
 }
 
-// Create a new QueryClient instance
 const queryClient = new QueryClient()
 
-// Find the root element in your HTML
 const rootElement = document.getElementById('root')
 
 if (!rootElement) {
@@ -28,7 +26,6 @@ ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       {' '}
-      {/* Wrap your app with QueryClientProvider */}
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
