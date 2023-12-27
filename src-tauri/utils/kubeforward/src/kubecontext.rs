f4efda23ca462eddc68f776d8ab1116a927046b6
hcavarsan <hcavarsan@yahoo.com.br>
2023-12-27 14:22:54 -0300

    feat: add autocomplete fields

--- src-tauri/utils/kubeforward/src/kubecontext.rs
+++ src-tauri/utils/kubeforward/src/kubecontext.rs
@@ -1,8 +1,7 @@
 use anyhow::Result;
-use kube::{
-    config::{Config, KubeConfigOptions},
-    Client,
-};
+use kube::{config::{Config, KubeConfigOptions, Kubeconfig}, Client, ResourceExt, api::{Api, ListParams}};
+use k8s_openapi::api::core::v1::{Service, Namespace};
+use serde::Serialize;
 
 pub async fn create_client_with_specific_context(context_name: &str) -> Result<Client> {
     let config_options = KubeConfigOptions {
@@ -14,4 +13,81 @@ pub async fn create_client_with_specific_context(context_name: &str) -> Result<C
     let config = Config::from_kubeconfig(&config_options).await?;
     let client = Client::try_from(config)?; // use try_from instead of from
     Ok(client)
+}
+
+
+#[derive(Serialize)]
+pub struct KubeContextInfo {
+    pub name: String,
+}
+
+#[derive(Serialize)]
+pub struct KubeNamespaceInfo {
+    pub name: String,
+}
+
+#[derive(Serialize)]
+pub struct KubeServiceInfo {
+    pub name: String,
+}
+
+#[derive(Serialize)]
+pub struct KubeServicePortInfo {
+    pub name: Option<String>,
+    pub port: u16,
+}
+
+#[tauri::command]
+pub async fn list_kube_contexts() -> Result<Vec<KubeContextInfo>, String> {
+    let kubeconfig = Kubeconfig::read().map_err(|e| e.to_string())?;
+    Ok(kubeconfig.contexts.into_iter().map(|c| KubeContextInfo { name: c.name }).collect())
+}
+
+#[tauri::command]
+pub async fn list_namespaces(context_name: &str) -> Result<Vec<KubeNamespaceInfo>, String> {
+    let client = create_client_with_specific_context(context_name).await
+        .map_err(|err| format!("Failed to create client for context '{}': {}", context_name, err))?;
+
+    let api: Api<Namespace> = Api::all(client);
+    let ns_list = api.list(&ListParams::default()).await.map_err(|e| e.to_string())?
+        .iter()
+        .map(|ns| KubeNamespaceInfo {
+            name: ns.name_any(),
+        })
+        .collect();
+
+    Ok(ns_list)
+}
+
+#[tauri::command]
+pub async fn list_services(context_name: &str, namespace: &str) -> Result<Vec<KubeServiceInfo>, String> {
+    let client = create_client_with_specific_context(context_name).await
+        .map_err(|err| format!("Failed to create client for context '{}': {}", context_name, err))?;
+
+    let api: Api<Service> = Api::namespaced(client, namespace);
+    let svc_list = api.list(&ListParams::default()).await.map_err(|e| e.to_string())?
+        .iter()
+        .map(|svc| KubeServiceInfo {
+            name: svc.name_any(),
+        })
+        .collect();
+
+    Ok(svc_list)
+}
+
+#[tauri::command]
+pub async fn list_service_ports(context_name: &str, namespace: &str, service_name: &str) -> Result<Vec<KubeServicePortInfo>, String> {
+    let client = create_client_with_specific_context(context_name).await
+        .map_err(|err| format!("Failed to create client for context '{}': {}", context_name, err))?;
+
+    let api: Api<Service> = Api::namespaced(client, namespace);
+    let svc = api.get(service_name).await.map_err(|e| e.to_string())?;
+    let service_ports = svc.spec.map_or(vec![], |spec| {
+        spec.ports.unwrap_or_default().into_iter().map(|p| KubeServicePortInfo {
+            name: p.name,
+            port: p.port as u16,
+        }).collect()
+    });
+
+    Ok(service_ports)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query' // Import QueryClient and QueryClientProvider
 import { attachConsole } from 'tauri-plugin-log-api'
 
 import { ChakraProvider } from '@chakra-ui/react'
@@ -12,10 +13,25 @@ import './assets/style.css'
 if (import.meta.env.DEV) {
   attachConsole()
 }
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+// Create a new QueryClient instance
+const queryClient = new QueryClient()
+
+// Find the root element in your HTML
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element')
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      {' '}
+      {/* Wrap your app with QueryClientProvider */}
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
