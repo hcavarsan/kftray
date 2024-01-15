a341d1cc087daf094d29c8f1f54b36e836061f5b
hcavarsan <hcavarsan@yahoo.com.br>
2024-01-15 11:51:14 -0300

    fix: some bugs in configs state

--- src-tauri/utils/kubeforward/src/proxy.rs
+++ src-tauri/utils/kubeforward/src/proxy.rs
@@ -67,7 +67,7 @@ pub async fn deploy_and_forward_pod(configs: Vec<Config>) -> Result<Vec<CustomRe
             "spec": {
                 "containers": [{
                     "name": hashed_name,
-                    "image": "ghcr.io/hcavarsan/kftray-server:v0.5.2",
+                    "image": "ghcr.io/hcavarsan/kftray-server:v0.5.3",
                     "env": [
                         {"name": "LOCAL_PORT", "value": config.remote_port.to_string()},
                         {"name": "REMOTE_PORT", "value": config.remote_port.to_string()},
