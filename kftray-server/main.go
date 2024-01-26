677170c66cd345351065eea761e7ff5bee494971
hcavarsan <hcavarsan@yahoo.com.br>
2024-01-26 20:27:32 -0300

    fix: new params in the relay lib

--- kftray-server/main.go
+++ kftray-server/main.go
@@ -31,11 +31,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("Invalid LOCAL_PORT: %s", proxyPortStr)
 	}
+	source := &netrelay.Address{Host: "0.0.0.0", Port: proxyPort}
+	destination := netrelay.NewRoundRobinPool([]*netrelay.Address{
+		{Host: targetHost, Port: targetPort},
+	})
 
 	switch proxyType {
 	case "tcp":
 		tcp := netrelay.NewTcpRelay()
-		err := tcp.Relay(proxyPort, targetPort, targetHost)
+		err := tcp.Relay(source, destination)
 		if err != nil {
 			log.Fatalf("Failed to start the TCP proxy server: %s", err)
 		}
