968738f0b29c8f52efc9fc0b9330c694118b7c4a
Henrique Cavarsan <hcavarsan@yahoo.com.br>
2024-02-08 21:50:28 -0300

    fix: bug to open app in linux and add global shortcut (#88)
    * fix: bug to open app in linux and add global shortcut
    
    * fix: bug to open app in linux and add global shortcut
    
    * fix: bug to open app in linux and add global shortcut
    
    * fix: bug to open app in linux and add global shortcut
    
    * fix: bug to open app in linux and add global shortcut
    
    * fix: bug to open app in linux and add global shortcut

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,9 @@ use log::LevelFilter;
 use std::env;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri::{
+    CustomMenuItem, GlobalShortcutManager, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+};
 use tauri_plugin_positioner::{Position, WindowExt};
 use tokio::runtime::Runtime;
 
@@ -57,7 +59,6 @@ fn setup_logging() {
 
         let log_file = OpenOptions::new()
             .create(true)
-            .write(true)
             .append(true)
             .open(log_path)
             .expect("Could not open log file");
@@ -78,11 +79,13 @@ fn setup_logging() {
 fn main() {
     setup_logging();
     let _ = fix_path_env::fix();
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
-    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("CmdOrCtrl+Shift+Q");
+    let open = CustomMenuItem::new("open".to_string(), "Open App");
+    let system_tray_menu = SystemTrayMenu::new().add_item(open).add_item(quit);
+
     tauri::Builder::default()
         .manage(SaveDialogState::default())
-        .setup(|_app| {
+        .setup(move |app| {
             db::init();
             if let Err(e) = config::migrate_configs() {
                 eprintln!("Failed to migrate configs: {}", e);
@@ -90,8 +93,30 @@ fn main() {
 
             #[cfg(target_os = "macos")]
             {
-                _app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+                app.set_activation_policy(tauri::ActivationPolicy::Accessory);
             }
+
+            let window = app.get_window("main").unwrap();
+
+            let mut shortcut = app.global_shortcut_manager();
+            shortcut
+                .register("CmdOrCtrl+Shift+F1", move || {
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        #[cfg(target_os = "linux")]
+                        let _ = window.move_window(Position::TopRight);
+                        #[cfg(target_os = "windows")]
+                        let _ = window.move_window(Position::BottomRight);
+                        #[cfg(target_os = "macos")]
+                        let _ = window.move_window(Position::TrayCenter);
+
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                })
+                .unwrap_or_else(|err| println!("{:?}", err));
+
             Ok(())
         })
         .plugin(tauri_plugin_positioner::init())
@@ -105,6 +130,11 @@ fn main() {
                     ..
                 } => {
                     let window = app.get_window("main").unwrap();
+                    #[cfg(target_os = "linux")]
+                    let _ = window.move_window(Position::TopRight);
+                    #[cfg(target_os = "windows")]
+                    let _ = window.move_window(Position::BottomRight);
+                    #[cfg(target_os = "macos")]
                     let _ = window.move_window(Position::TrayCenter);
 
                     if window.is_visible().unwrap() {
@@ -144,6 +174,17 @@ fn main() {
                         });
 
                         std::process::exit(0);
+                    }
+                    "open" => {
+                        let window = app.get_window("main").unwrap();
+                        #[cfg(target_os = "linux")]
+                        let _ = window.move_window(Position::TopRight);
+                        #[cfg(target_os = "windows")]
+                        let _ = window.move_window(Position::BottomRight);
+                        #[cfg(target_os = "macos")]
+                        let _ = window.move_window(Position::TrayCenter);
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
                     }
                     "hide" => {
                         let window = app.get_window("main").unwrap();
