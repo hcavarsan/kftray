202898a28efe9dba58c0068a414c3806c8f6078e
hcavarsan <hcavarsan@yahoo.com.br>
2024-02-19 18:18:45 -0300

    fix: lint

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { MdAdd, MdFileDownload, MdFileUpload, MdMoreVert } from 'react-icons/md'
+import { IoSettingsOutline } from 'react-icons/io5'
+import { MdAdd, MdFileDownload, MdFileUpload } from 'react-icons/md'
 
 import {
   Box,
-  Button,
-  Grid,
+  IconButton,
   Menu,
   MenuButton,
   MenuItem,
@@ -28,60 +28,57 @@ const MenuOptions: React.FC<MenuProps> = ({
   const handleSubmenuClose = () => setIsImportSubmenuOpen(false)
 
   return (
-    <Box justifyContent='space-between'>
-      <Grid templateColumns='repeat(2, 1fr)' gap={300} width='100%' mt={4}>
-        <Menu placement='top'>
-          <MenuButton
-            as={Button}
-            rightIcon={<MdMoreVert />}
-            size='xs'
-            colorScheme='facebook'
-            variant='outline'
-            borderRadius='md'
-            width='85px'
+    <Box display='flex' justifyContent='flex-start' width='100%' mt={4}>
+      <Menu placement='top'>
+        <MenuButton
+          as={IconButton}
+          aria-label='Options'
+          icon={<IoSettingsOutline />}
+          size='sm'
+          colorScheme='facebook'
+          variant='outline'
+        />
+        <MenuList onMouseLeave={handleSubmenuClose}>
+          <MenuItem icon={<MdAdd />} onClick={openModal}>
+            Add New Config
+          </MenuItem>
+          <MenuItem icon={<MdFileUpload />} onClick={handleExportConfigs}>
+            Export Configs
+          </MenuItem>
+          <Box
+            onMouseEnter={handleSubmenuOpen}
+            onMouseLeave={handleSubmenuClose}
+            position='relative'
           >
-            Options
-          </MenuButton>
-          <MenuList onMouseLeave={handleSubmenuClose}>
-            <MenuItem icon={<MdAdd />} onClick={openModal}>
-              Add New Config
-            </MenuItem>
-            <MenuItem icon={<MdFileUpload />} onClick={handleExportConfigs}>
-              Export Configs
-            </MenuItem>
-            <Box
-              onMouseEnter={handleSubmenuOpen}
-              onMouseLeave={handleSubmenuClose}
+            <Popover
+              isOpen={isImportSubmenuOpen}
+              placement='right-start'
+              closeOnBlur={false}
             >
-              <Popover
-                isOpen={isImportSubmenuOpen}
-                placement='right-start'
-                closeOnBlur={false}
+              <PopoverTrigger>
+                <MenuItem icon={<MdFileDownload />}>Import Configs</MenuItem>
+              </PopoverTrigger>
+              <PopoverContent
+                border='0'
+                boxShadow='none'
+                bg='transparent'
+                width='auto'
+                zIndex={1}
               >
-                <PopoverTrigger>
-                  <MenuItem icon={<MdFileDownload />}>Import Configs</MenuItem>
-                </PopoverTrigger>
-                <PopoverContent
-                  border='0'
-                  boxShadow='none'
-                  bg='transparent'
-                  width='auto'
+                <MenuList
+                  onMouseEnter={handleSubmenuOpen}
+                  onMouseLeave={handleSubmenuClose}
                 >
-                  <MenuList
-                    onMouseEnter={handleSubmenuOpen}
-                    onMouseLeave={handleSubmenuClose}
-                  >
-                    <MenuItem onClick={handleImportConfigs}>
-                      From Local File
-                    </MenuItem>
-                    <MenuItem onClick={openSettingsModal}>From Git</MenuItem>
-                  </MenuList>
-                </PopoverContent>
-              </Popover>
-            </Box>
-          </MenuList>
-        </Menu>
-      </Grid>
+                  <MenuItem onClick={handleImportConfigs}>
+                    From Local File
+                  </MenuItem>
+                  <MenuItem onClick={openSettingsModal}>From Git</MenuItem>
+                </MenuList>
+              </PopoverContent>
+            </Popover>
+          </Box>
+        </MenuList>
+      </Menu>
     </Box>
   )
 }
--- src/components/SettingsModal/index.tsx
+++ src/components/SettingsModal/index.tsx
@@ -187,7 +187,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
                   isLoading={isLoading}
                   isDisabled={isLoading}
                 >
-                  Save Settings
+                  Import Configs
                 </Button>
               </ModalFooter>
             </form>
