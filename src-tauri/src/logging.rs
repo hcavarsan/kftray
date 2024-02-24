a8fbf04ead172bf97471eca47eb1e3395cddbf79
hcavarsan <hcavarsan@yahoo.com.br>
2024-02-23 21:27:10 -0300

    chore: add initial comments

--- src-tauri/src/logging.rs
+++ src-tauri/src/logging.rs
@@ -2,6 +2,7 @@ use log::LevelFilter;
 use std::{env, fs::OpenOptions, io, path::PathBuf};
 use thiserror::Error;
 
+/// Defines the types of errors that can occur when trying to set up logging.
 #[derive(Debug, Error)]
 pub enum LogError {
     #[error("home directory could not be determined")]
@@ -17,20 +18,28 @@ pub enum LogError {
     LogFileOpenFailed(io::Error),
 }
 
+/// Gets the path to the log file.
 pub fn get_log_path() -> Result<PathBuf, LogError> {
+    // Retrieve the home directory or return a `HomeDirNotFound` error.
     let home_dir = dirs::home_dir().ok_or(LogError::HomeDirNotFound)?;
+    // Construct and return the path to the log file.
     Ok(home_dir.join(".kftray").join("app.log"))
 }
 
+/// Sets up the logging environment using `env_logger`.
 pub fn setup_logging() -> Result<(), LogError> {
+    // Get the log filter level from the environment variable or fall back to `LevelFilter::Off`.
     let log_filter = env::var("RUST_LOG")
         .map(|filter| filter.parse().unwrap_or(LevelFilter::Info))
         .unwrap_or(LevelFilter::Off);
 
     let mut builder = env_logger::builder();
+    // Configure the builder's filter level and timestamp format.
     builder.filter_level(log_filter).format_timestamp_secs();
 
+    // If the `KFTRAY_DEBUG` environment variable is set, set up file-based logging.
     if env::var("KFTRAY_DEBUG").is_ok() {
+        // Get the log path, or return an error if it fails.
         let log_path = get_log_path()?;
         let log_dir = log_path
             .parent()
@@ -39,17 +48,21 @@ pub fn setup_logging() -> Result<(), LogError> {
                 "Log directory path cannot be determined",
             )))?;
 
+        // Try to create the log directory, if it does not already exist.
         std::fs::create_dir_all(log_dir).map_err(LogError::LogDirCreationFailed)?;
 
+        // Open the log file for appending, or return an error if it fails.
         let log_file = OpenOptions::new()
             .create(true)
             .append(true)
             .open(log_path)
             .map_err(LogError::LogFileOpenFailed)?;
 
+        // Set the target of the logger to the log file.
         builder.target(env_logger::Target::Pipe(Box::new(log_file)));
     }
 
+    // Initialize the logging infrastructure.
     builder.init();
 
     Ok(())
