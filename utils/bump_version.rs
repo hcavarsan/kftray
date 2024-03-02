fdba6d2009745a993bda02b85ce6f395f5647361
hcavarsan <hcavarsan@yahoo.com.br>
2024-03-02 10:45:04 -0300

    feat: improve overall scripts usage and use rust to automated task utils

--- utils/bump_version.rs
+++ utils/bump_version.rs
@@ -1,6 +1,6 @@
 use log::{error, info};
 use regex::Regex;
-use serde_json::{json, Value};
+use serde_json::Value;
 use std::{
     env, fs, io,
     process::{Command, ExitCode},
@@ -8,128 +8,154 @@ use std::{
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
-    const USAGE: &str = "Usage: bump_version [patch|minor|major]";
+    const USAGE: &str = "Usage: bump_version <patch|minor|major>";
+    const INVALID_ARGUMENT: &str =
+        "Invalid argument provided. Expecting 'patch', 'minor' or 'major'.";
 
     if args.len() != 2 {
-        error!("{}", USAGE.replace("bump_version", &args[0]));
+        error!(
+            "Incorrect number of arguments. {}",
+            USAGE.replace("bump_version", &args[0])
+        );
+        return ExitCode::from(1);
+    }
+
+    if !matches!(args[1].as_str(), "patch" | "minor" | "major") {
+        error!(
+            "{} {}",
+            INVALID_ARGUMENT,
+            USAGE.replace("bump_version", &args[0])
+        );
         return ExitCode::from(1);
     }
 
     match bump_version(&args[1]) {
-        Ok(()) => ExitCode::SUCCESS,
+        Ok(()) => {
+            info!("Version bump completed successfully.");
+            ExitCode::SUCCESS
+        }
         Err(e) => {
-            error!("Error: {:?}", e);
+            error!("Version bump failed: {}", e);
             ExitCode::from(1)
         }
     }
 }
 
 fn bump_version(bump_type: &str) -> io::Result<()> {
+    log::info!("Bumping version to {}", bump_type);
     let npm_output = Command::new("npm")
         .args(["version", bump_type, "--no-git-tag-version"])
         .output()?;
 
     if !npm_output.status.success() {
-        return Err(io::Error::new(
-            io::ErrorKind::Other,
-            String::from_utf8_lossy(&npm_output.stderr),
-        ));
+        let error_output = String::from_utf8_lossy(&npm_output.stderr).to_string();
+        return Err(io::Error::new(io::ErrorKind::Other, error_output));
     }
-
-    let output_str = String::from_utf8_lossy(&npm_output.stdout);
-    let new_version_tag = output_str.lines().last().unwrap_or_default().trim();
-    let new_version = new_version_tag.strip_prefix('v').unwrap_or(new_version_tag);
-
-    info!("NPM version successfully bumped to {}", new_version);
-
-    update_cargo_toml_version("src-tauri/Cargo.toml", new_version)?;
-    update_readme_md_version("README.md", new_version)?;
-    update_tauri_conf_version("src-tauri/tauri.conf.json", new_version)?;
-    git_tag(new_version)?;
-
-    info!("Version updated in Cargo.toml and README.md");
-    println!("Version bumped to {}", new_version);
-    Ok(())
-}
-
-fn update_cargo_toml_version(cargo_toml_path: &str, new_version: &str) -> io::Result<()> {
-    let data = fs::read_to_string(cargo_toml_path)?;
-    let mut lines: Vec<_> = data.lines().map(String::from).collect();
-
-    let package_section_regex = Regex::new(r"^\[package\]\s*$")
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let version_regex = Regex::new(r#"^version = "\d+\.\d+\.\d+""#)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let mut in_package_section = false;
-
-    for line in &mut lines {
-        if package_section_regex.is_match(line) {
-            in_package_section = true;
-        } else if line.starts_with('[') && line.ends_with(']') {
-            in_package_section = false;
-        }
-
-        if in_package_section && version_regex.is_match(line) {
-            *line = format!(r#"version = "{}""#, new_version);
-            break;
-        }
-    }
-
-    fs::write(cargo_toml_path, lines.join("\n").as_bytes())?;
-
-    Ok(())
-}
-
-fn update_readme_md_version(readme_md_path: &str, new_version: &str) -> io::Result<()> {
-    let data = fs::read_to_string(readme_md_path)?;
-    let version_regex = Regex::new(r"kftray_\d+\.\d+\.\d+")
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let updated_data = version_regex
-        .replace_all(&data, format!("kftray_{}", new_version))
+    let new_version_tag = String::from_utf8(npm_output.stdout)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+        .lines()
+        .last()
+        .unwrap_or_default()
+        .trim()
         .to_string();
+    let new_version = new_version_tag
+        .strip_prefix('v')
+        .unwrap_or(&new_version_tag);
+    log::info!("NPM version successfully bumped to: {}", new_version);
+    info!("NPM version successfully bumped to: {}", new_version);
 
-    fs::write(readme_md_path, updated_data.as_bytes())?;
+    info!("Updating version in Cargo.toml, README.md and tauri.conf.json");
+    update_file_content(
+        "src-tauri/Cargo.toml",
+        new_version,
+        update_cargo_toml_version,
+    )?;
+    log::info!("Cargo.toml updated");
+    update_file_content("README.md", new_version, update_markdown_version)?;
+    log::info!("README.md updated");
+    update_file_content(
+        "src-tauri/tauri.conf.json",
+        new_version,
+        update_json_version,
+    )?;
+    log::info!("tauri.conf.json updated");
+    git_tag(new_version)?;
+    log::info!("Git tag and push completed");
+    log::info!("All versions updated to: {}", new_version);
     Ok(())
 }
 
-fn update_tauri_conf_version(tauri_conf_path: &str, new_version: &str) -> io::Result<()> {
-    let content = fs::read_to_string(tauri_conf_path)?;
+fn update_file_content<F>(file_path: &str, new_version: &str, update_fn: F) -> io::Result<()>
+where
+    F: Fn(&str, &str) -> io::Result<String>,
+{
+    let content = fs::read_to_string(file_path)?;
+    let updated_content = update_fn(&content, new_version)?;
+    fs::write(file_path, updated_content)?;
+    Ok(())
+}
 
-    let mut json_content: Value = serde_json::from_str(&content)
+fn update_cargo_toml_version(content: &str, new_version: &str) -> io::Result<String> {
+    let package_section_regex = Regex::new(r"^\[package\]\s*$")
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    let version_regex = Regex::new(r#"version = "\d+\.\d+\.\d+""#)
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
+    let mut in_package_section = false;
+    let mut updated_lines = Vec::new();
+
+    for line in content.lines() {
+        if package_section_regex.is_match(line) {
+            in_package_section = true;
+        } else if line.starts_with('[') {
+            in_package_section = false;
+        }
+        if in_package_section && version_regex.is_match(line) {
+            updated_lines.push(format!(r#"version = "{}""#, new_version));
+            in_package_section = false; // Not sure if you want to set this to false here
+        } else {
+            updated_lines.push(line.to_string());
+        }
+    }
+    Ok(updated_lines.join("\n"))
+}
+
+fn update_markdown_version(content: &str, new_version: &str) -> io::Result<String> {
+    let version_regex = Regex::new(r"kftray_\d+\.\d+\.\d+")
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    Ok(version_regex
+        .replace_all(content, format!("kftray_{}", new_version))
+        .into_owned())
+}
+
+fn update_json_version(content: &str, new_version: &str) -> io::Result<String> {
+    let mut json_content: Value =
+        serde_json::from_str(content).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
     if let Some(package) = json_content.get_mut("package") {
-        package["version"] = json!(new_version);
+        package["version"] = serde_json::Value::String(new_version.to_string());
     } else {
         return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
+            io::ErrorKind::NotFound,
             "Missing 'package' section in tauri.conf.json",
         ));
     }
 
-    fs::write(
-        tauri_conf_path,
-        serde_json::to_string_pretty(&json_content)?.as_bytes(),
-    )?;
-
-    Ok(())
+    serde_json::to_string_pretty(&json_content)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
 }
 
-fn git_tag(new_version: &str) -> std::io::Result<()> {
-    let git_output = Command::new("git")
-        .args(["tag", "-f", &format!("v{}", new_version)])
+fn git_tag(new_version: &str) -> io::Result<()> {
+    let tag_name = format!("v{}", new_version);
+    let git_tag_output = Command::new("git")
+        .args(["tag", "-f", &tag_name])
         .output()?;
 
-    if !git_output.status.success() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            String::from_utf8_lossy(&git_output.stderr),
+    if !git_tag_output.status.success() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            String::from_utf8_lossy(&git_tag_output.stderr).into_owned(),
         ));
     }
-
-    Command::new("git")
-        .args(["push", "origin", &format!("v{}", new_version), "--force"])
-        .output()?;
-
     Ok(())
 }
--- utils/generate_icons.rs
+++ utils/generate_icons.rs
@@ -1,12 +1,23 @@
 use image::io::Reader as ImageReader;
-use image::{imageops::FilterType, DynamicImage};
+use image::{DynamicImage, ImageError, ImageFormat};
 use std::fs::{self, File};
-use std::path::Path;
+use std::io::Error as IoError;
+use std::path::PathBuf;
+use std::process::Command;
+use thiserror::Error;
 
 const SRC_FILE: &str = "./img/logo.png";
 const DST_PATH: &str = "./src-tauri/icons";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Debug, Error)]
+enum CustomError {
+    #[error("Image error: {0}")]
+    ImageError(#[from] ImageError),
+    #[error("IO error: {0}")]
+    IoError(#[from] IoError),
+}
+
+fn main() -> Result<(), CustomError> {
     fs::create_dir_all(DST_PATH)?;
 
     println!("Info: Generating Icons");
@@ -19,76 +30,70 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn generate_png_icons() -> Result<(), Box<dyn std::error::Error>> {
+fn generate_png_icons() -> Result<(), CustomError> {
     println!("Info: Generating PNG icons ...");
-    let sizes = [32, 128, 256, 512];
-    let src = ImageReader::open(SRC_FILE)?.decode()?;
+    let sizes = [16, 24, 32, 48, 64, 128, 256, 512, 1024];
+    let src = ImageReader::open(SRC_FILE)?
+        .with_guessed_format()?
+        .decode()?;
     for size in sizes.iter() {
         resize_and_save(
             &src,
             *size,
             *size,
-            &format!("{}/{}x{}.png", DST_PATH, size, size),
+            &PathBuf::from(DST_PATH).join(format!("icon_{}x{}.png", size, size)),
         )?;
     }
-    resize_and_save(&src, 256, 256, &format!("{}/128x128@2x.png", DST_PATH))?;
     Ok(())
 }
 
-fn generate_icns() -> Result<(), Box<dyn std::error::Error>> {
+fn generate_icns() -> Result<(), CustomError> {
     println!("Info: Generating icon.icns ...");
-    let icns_path = Path::new(DST_PATH).join("icon.iconset");
+    let icns_path = PathBuf::from(DST_PATH).join("icon.iconset");
     fs::create_dir_all(&icns_path)?;
 
-    let sizes = [16, 32, 128, 256, 512, 1024];
+    let sizes = [16, 32, 64, 128, 256, 512, 1024];
     let src = ImageReader::open(SRC_FILE)?.decode()?;
     for size in sizes.iter() {
         let factor = if *size > 16 { 2 } else { 1 };
         let actual_size = size / factor;
         let file_name = if factor > 1 {
-            format!("icon_{}x{}@{}x.png", actual_size, actual_size, factor)
+            format!("icon_{}x{}@2x.png", actual_size, actual_size)
         } else {
             format!("icon_{}x{}.png", size, size)
         };
-        resize_and_save(
-            &src,
-            *size,
-            *size,
-            icns_path.join(file_name).to_str().unwrap(),
-        )?;
+        resize_and_save(&src, *size, *size, &icns_path.join(&file_name))?;
     }
 
-    std::process::Command::new("iconutil")
-        .args([
-            "-c",
-            "icns",
-            icns_path.to_str().unwrap(),
-            "-o",
-            &format!("{}/icon.icns", DST_PATH),
-        ])
-        .spawn()?
-        .wait()?;
+    Command::new("iconutil")
+        .arg("-c")
+        .arg("icns")
+        .arg(&icns_path)
+        .arg("-o")
+        .arg(PathBuf::from(DST_PATH).join("icon.icns"))
+        .status()?;
 
     fs::remove_dir_all(icns_path)?;
     Ok(())
 }
 
-fn generate_ico() -> Result<(), Box<dyn std::error::Error>> {
+fn generate_ico() -> Result<(), CustomError> {
     println!("Info: Generating icon.ico ...");
 
     let src = ImageReader::open(SRC_FILE)?.decode()?;
     let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
-    let sizes = [16, 24, 32, 48, 64, 256, 512];
+    let sizes = [16, 32, 48, 64, 128, 256];
 
-    for size in sizes.iter() {
-        let resized = src.resize_exact(*size, *size, FilterType::Lanczos3);
+    for &size in &sizes {
+        let resized = src.resize_exact(size, size, image::imageops::FilterType::Lanczos3);
         let rgba = resized.to_rgba8();
         let (width, height) = rgba.dimensions();
         let ico_image = ico::IconImage::from_rgba_data(width, height, rgba.into_raw());
         icon_dir.add_entry(ico::IconDirEntry::encode(&ico_image)?);
     }
 
-    let file_out = File::create(format!("{}/icon.ico", DST_PATH))?;
+    let file_path = PathBuf::from(DST_PATH).join("icon.ico");
+    let file_out = File::create(file_path)?;
     icon_dir.write(file_out)?;
 
     Ok(())
@@ -98,9 +103,9 @@ fn resize_and_save(
     src: &DynamicImage,
     width: u32,
     height: u32,
-    save_path: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    src.resize(width, height, FilterType::Lanczos3)
-        .save(save_path)?;
+    save_path: &PathBuf,
+) -> Result<(), CustomError> {
+    src.resize(width, height, image::imageops::FilterType::Lanczos3)
+        .save_with_format(save_path, ImageFormat::Png)?;
     Ok(())
 }
