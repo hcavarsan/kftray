b03ebb2556f9c7472444c23f3612d07a2c06b706
hcavarsan <hcavarsan@yahoo.com.br>
2024-04-09 22:19:37 -0300

    fix: fix arrow position in each OS

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -2,7 +2,7 @@ use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
 
 pub fn create_tray_menu() -> SystemTray {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("CmdOrCtrl+Shift+Q");
-    let open = CustomMenuItem::new("open".to_string(), "Open App");
+    let open = CustomMenuItem::new("toggle".to_string(), "Toggle App");
     let system_tray_menu = SystemTrayMenu::new().add_item(open).add_item(quit);
     SystemTray::new().with_menu(system_tray_menu)
 }
