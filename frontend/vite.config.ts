2f4705305d043ff97d4e98e42bd82d6ff4159dcc
hcavarsan <hcavarsan@yahoo.com.br>
2024-05-24 12:43:32 -0300

    feat: improved stop logic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 
+import terser from '@rollup/plugin-terser'
 import react from '@vitejs/plugin-react-swc'
+
+console.log('TAURI_DEBUG:', process.env.TAURI_DEBUG)
 
 export default defineConfig({
   resolve: {
@@ -9,7 +12,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    !process.env.TAURI_DEBUG && terser({
+      compress: {
+        drop_console: true,
+      },
+    }),
+  ].filter(Boolean),
   clearScreen: false,
   server: {
     port: 1420,
@@ -20,7 +30,21 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 600,
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_DEBUG ? 'terser' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const chunks = id.toString().split('node_modules/')[1].split('/')
+
+
+            if (chunks.length > 1 && chunks[0] !== '') {
+              return chunks[0]
+            }
+          }
+        },
+      },
+    },
   },
 })
