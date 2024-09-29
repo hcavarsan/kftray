566d044fd5f2087361ade53f800333f60d8725d4
Henrique Cavarsan <hcavarsan@yahoo.com.br>
2024-09-29 19:08:07 -0300

    deps: update some packages also use mise (rust 1.81 and node 22)  (#316)
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * update dependencies and versions
    
    * chore: bump version
    
    * chore: bump version

--- frontend/src/components/AddConfigModal/index.tsx
+++ frontend/src/components/AddConfigModal/index.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/jsx-no-duplicate-props */
 /* eslint-disable complexity */
-/* eslint-disable max-len */
+
 import React, { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import ReactSelect, { ActionMeta } from 'react-select'
