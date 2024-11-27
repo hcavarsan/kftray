60c47cd2f5eb54a7dbb35a1b14208a89ac918f76
Henrique Cavarsan <hcavarsan@yahoo.com.br>
2024-11-27 11:09:30 -0300

    feat: add pin window to systray menu (#344)
    * feat: add pin window to systray menu
    
    * fix: change state in toggle_pin_state to better consistency
    
    * fix: more simple code

--- crates/kftray-tauri/src/commands/window_state.rs
+++ crates/kftray-tauri/src/commands/window_state.rs
@@ -2,6 +2,8 @@ use std::sync::atomic::Ordering;
 
 use kftray_commons::models::window::AppState;
 use kftray_commons::models::window::SaveDialogState;
+use log::error;
+use tauri::Manager;
 use tauri::State;
 
 #[tauri::command]
@@ -16,10 +18,34 @@ pub fn close_save_dialog(state: State<SaveDialogState>) {
 
 #[tauri::command]
 pub fn toggle_pin_state(app_state: tauri::State<AppState>, window: tauri::Window) {
-    let is_pinned = app_state.is_pinned.load(Ordering::SeqCst);
-    app_state.is_pinned.store(!is_pinned, Ordering::SeqCst);
+    let new_pin_state = !app_state.is_pinned.load(Ordering::SeqCst);
+    app_state.is_pinned.store(new_pin_state, Ordering::SeqCst);
 
-    if let Err(e) = window.set_always_on_top(!is_pinned) {
-        eprintln!("Failed to toggle pin state: {:?}", e);
+    if new_pin_state && !window.is_visible().unwrap_or(true) {
+        let _ = window.show();
+        let _ = window.set_focus();
+    }
+
+    if let Err(e) = window.set_always_on_top(new_pin_state) {
+        error!("Failed to toggle pin state: {:?}", e);
+    }
+
+    let menu_title = if new_pin_state {
+        "Unpin Window"
+    } else {
+        "Pin Window"
+    };
+
+    if let Err(e) = window
+        .app_handle()
+        .tray_handle()
+        .get_item("pin")
+        .set_title(menu_title)
+    {
+        error!("Failed to update menu item text: {:?}", e);
+    }
+
+    if let Err(e) = window.emit("pin-state-changed", new_pin_state) {
+        error!("Failed to emit pin state event: {:?}", e);
     }
 }
